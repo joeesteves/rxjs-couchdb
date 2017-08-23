b830ab25bef117a105f399de2a1c12e49b066cc8
ponyesteves <ajesteves@gmail.com>
2017-08-23 17:42:45 -0300

    add @types

---
+++ dist/index.d.ts
@@ -0,0 +1,14 @@
+import Rx from 'rxjs'
+interface IInit {
+  find: (selector) => Rx.Observable<{response: any, body: any}>
+  get: (selector) => Rx.Observable<{response: any, body: any}>
+  put: (selector) => Rx.Observable<{response: any, body: any}>
+  bulk: (selector) => Rx.Observable<{response: any, body: any}>
+}
+interface DB_HEADERS {
+  'Content-Type': String
+  'Authorization': String
+  'User-Agent': String
+}
+
+export const init: (DBURI: String, headers: DB_HEADERS) => IInit
